2dd0e551beadf42164528ed782dab53331aa9f93
Kushagra Gangwar <kgang6434@gmail.com>
2024-08-12 17:56:38 +0530

    new changes

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ app.register_blueprint(user_bp)
 app.register_blueprint(contact_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

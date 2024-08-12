87ebee255ed06a8bbb3fb9c873534d4e89559812
Kushagra Gangwar <kgang6434@gmail.com>
2024-08-12 17:38:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import Config
+from models import db, bcrypt
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+bcrypt.init_app(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+from routes.user_routes import user_bp
+from routes.contact_routes import contact_bp
+
+app.register_blueprint(user_bp)
+app.register_blueprint(contact_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)

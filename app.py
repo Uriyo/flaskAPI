7214eeeb03ac48725555311901c4bc2e97f49bb2
Kushagra Gangwar <kgang6434@gmail.com>
2024-08-18 19:29:18 +0530

    vercel deploy

--- app.py
+++ app.py
@@ -5,20 +5,18 @@ from models import db, bcrypt
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    migrate = Migrate(app, db)
+    jwt = JWTManager(app)
 
-db.init_app(app)
-bcrypt.init_app(app)
-migrate = Migrate(app, db)
-jwt = JWTManager(app)
+    from routes.user_routes import user_bp
+    from routes.contact_routes import contact_bp
 
-from routes.user_routes import user_bp
-from routes.contact_routes import contact_bp
+    app.register_blueprint(user_bp)
+    app.register_blueprint(contact_bp)
 
-app.register_blueprint(user_bp)
-app.register_blueprint(contact_bp)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    return app

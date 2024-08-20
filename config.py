2ecb1dbe8334e0e63ebf52b9106fc13c193c6f43
Kushagra Gangwar <kgang6434@gmail.com>
2024-08-20 13:22:18 +0530

    gunicorn installation

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
-
-    
